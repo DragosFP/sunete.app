5a0e2900dda5c252929f09e6fe6d178e534adf0b
Dragos Florin <pshopelectronics@gmail.com>
2016-10-26 20:31:04 +0300

    Final

--- Sunete - iOS 10/AdaugaSunetViewController.swift
+++ Sunete - iOS 10/AdaugaSunetViewController.swift
@@ -7,29 +7,110 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AdaugaSunetViewController: UIViewController {
+    @IBOutlet weak var butonInregistreaza: UIButton!
+    @IBOutlet weak var butonAsculta: UIButton!
+    @IBOutlet weak var campText: UITextField!
+    @IBOutlet weak var butonSalveaza: UIButton!
+    
+    var inregistrareAudio : AVAudioRecorder?
+    var asculta : AVAudioPlayer?
+    var sunetURL : URL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setariInregistrare()
+        butonAsculta.isEnabled = false
+        butonSalveaza.isEnabled = false
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setariInregistrare() {
+        
+        do {
+            // Create an audio session
+            
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord) //ne permite sa play and record
+            try session.overrideOutputAudioPort(.speaker) // ne permite sa ascultam la difuzoare
+            try session.setActive(true)
+            
+            
+            //Create URL for the audio file
+            
+            let basePath : String  = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            
+            let pathComponente = [basePath, "sunet.m4a"]
+            
+            sunetURL = NSURL.fileURL(withPathComponents: pathComponente)!
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+            print(sunetURL)
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+            
+            //Create settings for audio recorder
+            
+            var setari : [String:AnyObject] = [:]
+            setari[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            setari[AVSampleRateKey] = 44100.0 as AnyObject?
+            setari[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            // Create AudioRecorder (inregistrareAudio) object
+            
+            inregistrareAudio =  try AVAudioRecorder (url: sunetURL!, settings: setari)
+            inregistrareAudio!.prepareToRecord()
+            
+            
+        } catch  let error as NSError{
+            print (error)
+        }
     }
-    */
 
+
+    @IBAction func inregistreazaApasat(_ sender: AnyObject) {
+        if inregistrareAudio!.isRecording{
+            // Stop the recording
+            inregistrareAudio?.stop()
+            
+            //Schimba butonul in Inregistreaza
+            butonInregistreaza.setTitle("Inregistreaza", for: .normal)
+            
+            butonAsculta.isEnabled = true
+            butonSalveaza.isEnabled = true
+        }else {
+            // Start the recording
+            inregistrareAudio?.record()
+            
+            //Schimba butonul in Stop
+            butonInregistreaza.setTitle("Stop", for: .normal)
+        }
+    }
+    
+    @IBAction func ascultaApasat(_ sender: AnyObject) {
+        //Setare Audio Player
+        do{
+            try asculta = AVAudioPlayer(contentsOf: sunetURL!)
+            asculta!.play()
+        }catch {}
+
+    } 
+    
+    @IBAction func salveazaApasat(_ sender: AnyObject) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let sound = Sunet(context: context)
+        
+        sound.nume = campText.text
+        
+        sound.audio = NSData(contentsOf: sunetURL!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+        
+    }
+    
+    
 }
--- Sunete - iOS 10/PrimaPaginaViewController.swift
+++ Sunete - iOS 10/PrimaPaginaViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class PrimaPaginaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sunete : [Sunet] = []
+    var asculta : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            try sunete = context.fetch(Sunet.fetchRequest())
+            tableView.reloadData()
+        }catch{}
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sunete.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let linie = UITableViewCell()
+        
+        let sunet = sunete [indexPath.row]
+        
+        linie.textLabel?.text = sunet.nume
+        
+        return linie
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sunet = sunete [indexPath.row]
+        do{
+            asculta = try AVAudioPlayer(data: sunet.audio! as Data)
+            asculta?.play()
+            } catch {}
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+        let sunet = sunete [indexPath.row]
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(sunet)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do{
+                try sunete = context.fetch(Sunet.fetchRequest())
+                tableView.reloadData()
+            }catch{}
+        }
+    }
+    
 }
-
